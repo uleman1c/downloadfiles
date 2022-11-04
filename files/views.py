2891c5dba56042ebfe3a8f8bbe7d25b0f1d57cc5
Mihail <uleman@mail.ru>
2022-11-04 18:15:10 +0300

    files end

--- files/views.py
+++ files/views.py
@@ -1,5 +1,9 @@
+import json
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse, JsonResponse
+import requests
+
+import urllib.parse
 
 # Create your views here.
 
@@ -7,5 +11,21 @@ def files(request, slug):
 
     d = 1
 
-    # return HttpResponse(slug)
-    return redirect('http://localhost:8008/el?id=' + slug)
+    res = requests.get('http://localhost:8000/eln?id=' + slug)
+
+    filespath = 'I:\\Files\\'
+
+    resdict = json.loads(res.text)
+    
+    
+
+    fr = FileResponse(open(filespath + resdict['id'] + ".tmp",'rb'))
+
+    fr['Content-Disposition'] = 'attachment; filename=' + urllib.parse.quote(resdict['filename'].encode('utf8'))
+    fr['X-Sendfile'] = urllib.parse.quote(resdict['filename'].encode('utf8'))
+
+
+    return fr
+
+    # return JsonResponse(json.loads(res.text))
+    # return redirect('http://localhost:8000/el?id=' + slug)
